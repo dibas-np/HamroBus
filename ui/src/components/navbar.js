7461d80370269be9cf3c1ffacfa026d82d9602dc
dibas-np <np03s200002@heraldcollege.edu.np>
2022-03-04 19:15:29 +0545

    added navigation bar (#8)

--- ui/src/components/navbar.js
+++ ui/src/components/navbar.js
@@ -1,47 +1,55 @@
-import React from "react";
-import { Nav, NavLink, NavMenu }
+import React , { useEffect, useState } from "react";
+import { NavLink, NavMenu }
 from "./NavbarElements";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import API from "./API";
 
-const Navbar = () => {
+const NavigationBar = () => {
+  const [username, setUsername] = useState("Profile");
+  useEffect(() => {
+    getUsername();
+  }, []);
+  const getUsername = () => {
+    API.get("loggeduser/1/").then(res => {
+      setUsername(res.data.username);
+    })
+
+  }
 return (
-<>
-    <Nav>
-        <NavMenu>
-            < NavLink to = "/home"
-            activeStyle = {
-                {
-                    color: 'red'
-                }
-            } >
-                Home
-            </NavLink>
-            < NavLink to = "/route"
-            activeStyle = {
-                {
-                    color: 'red'
-                }
-            } >
-                Add Route
-            </NavLink>
-
-            <NavLink to="/ticket"
-            activeStyle = {
-                {
-                    color: 'red'
-                }
-            } >
-                Tickets
-            </NavLink>
-            {/* <NavLink to="/blogs" activeStyle>
-                Blogs
-            </NavLink>
-            <NavLink to="/sign-up" activeStyle>
-                Sign Up
-            </NavLink> */}
-        </NavMenu>
-    </Nav>
-</>
-);
+<Navbar  bg="dark" expand="lg" variant="dark">
+  <Container>
+    <Navbar.Brand href="/home">
+        <img
+          alt=""
+          src = "https://cdn.discordapp.com/attachments/591659008775946240/949292011624726568/Untitled_design_3.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '} 
+        Hamro Bus</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="/home">Home</Nav.Link>
+        <Nav.Link href="/route">Add Route</Nav.Link>
+        <Nav.Link href="/ticket">Manage Tickets</Nav.Link>
+        <Nav.Link href="#">About Us</Nav.Link>
+        <Nav.Link href="#">Contact</Nav.Link>
+        <NavDropdown title= {"Hello, "+ username} id="basic-nav-dropdown">
+          <NavDropdown.Item href="/ticket">Your Tickets</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="/logout/">Log out</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+)
 };
 
-export default Navbar;
+export default NavigationBar;
